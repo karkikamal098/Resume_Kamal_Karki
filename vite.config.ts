68d35ac917ec11c24329ba1481c591ce538c5f43
Kamal Karki <kamaljungkarki13579@gmail.com>
2025-07-21 16:02:32 +0545

    host from the github arrange

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // This is the crucial line for GitHub Pages deployment.
+  // It tells Vite that your project will be in a subfolder.
   base: "/Resume_Kamal_Karki/",
   server: {
     host: "::",

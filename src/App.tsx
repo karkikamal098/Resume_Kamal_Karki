464eae2b9e2a9b4dfaa510c18c1a7b5746013552
Kamal Karki <119439311+karkikamal098@users.noreply.github.com>
2025-08-25 22:03:34 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Resume_Kamal_Karki">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
